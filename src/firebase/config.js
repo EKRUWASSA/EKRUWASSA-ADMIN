28f89fa83cc0cecf7a7a03863d7e2ba3e1147700
simi <108358542+simi-ajayi@users.noreply.github.com>
2025-01-20 18:11:29 +0100

    fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 
-import firebase from "firebase";
+// import firebase from "firebase";
+import firebase from "firebase/app";
+
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
