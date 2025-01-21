f67b660f195c275e0394ab565d4046f3ce8e740b
simi <108358542+simi-ajayi@users.noreply.github.com>
2025-01-21 15:47:13 +0100

    run dev fix

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,23 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuild: {
+      loader: { '.js': 'jsx' }  // This is crucial for handling JSX in .js files
+    }
+  },
   esbuild: {
-    loader: 'jsx', // Set the default loader to handle JSX
-    include: /\.(js|jsx)$/, // Apply to both .js and .jsx files
-    exclude: /node_modules/, // Exclude node_modules for performance
+    loader: 'jsx',
+    include: /\.(js|jsx)$/,
+    exclude: /node_modules/,
   },
   resolve: {
-    extensions: ['.js', '.jsx'], // Ensure these extensions are resolved
+    extensions: ['.js', '.jsx']
   },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
+  }
 });
